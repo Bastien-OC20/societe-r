a8012b6bfd4817ca79a9b0c626a00781bec06ff3
SelNWK <marcel.1999@live.fr>
2021-12-17 11:42:29 +0100

    ajout entite workteam

---
+++ src/Entity/WorkTeam.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\WorkTeamRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: WorkTeamRepository::class)]
+class WorkTeam
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 50, nullable: true)]
+    private $name;
+
+    #[ORM\Column(type: 'string', length: 50, nullable: true)]
+    private $color;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getColor(): ?string
+    {
+        return $this->color;
+    }
+
+    public function setColor(?string $color): self
+    {
+        $this->color = $color;
+
+        return $this;
+    }
+}
