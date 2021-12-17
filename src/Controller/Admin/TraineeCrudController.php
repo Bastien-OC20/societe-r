243758353e1cedead2570f5616136df4fccfb03f
Hamada Sidi Fahari <mkl.devops@gmail.com>
2021-12-17 12:58:19 +0000

    fix Paginator#setUseOutputWalkers(true)

--- src/Controller/Admin/TraineeCrudController.php
+++ src/Controller/Admin/TraineeCrudController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Trainee;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class TraineeCrudController extends AbstractCrudController
@@ -10,6 +11,13 @@ class TraineeCrudController extends AbstractCrudController
     public static function getEntityFqcn(): string
     {
         return Trainee::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setPaginatorUseOutputWalkers(true)
+            ;
     }
 
     /*
